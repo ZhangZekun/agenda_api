156a3fe08deba3701578c0292cedc54071b36f2b
ZhangZekun <13829062426@163.com>
2017-12-18 20:36:28 +0800

    add database operation

---
+++ server/databaseService/initDatabase.go
@@ -0,0 +1,24 @@
+package databaseService
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
+	"github.com/go-xorm/xorm"
+	"time"
+)
+
+var Enginea *xorm.Engine
+
+func init() {
+	//https://stackoverflow.com/questions/45040319/unsupported-scan-storing-driver-value-type-uint8-into-type-time-time
+	Enginea, _ = xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/agenda?charset=utf8")
+	err := Enginea.Ping()
+	CheckErr(err)
+	Enginea.SetMapper(core.SameMapper{})
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
