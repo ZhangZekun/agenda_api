e01c8e80bf17ed30485fef9f74eac7d8288c9aed
ZhangZeMian <1106066690@qq.com>
2017-12-18 18:13:04 +0800

    server和hanler接口定义

---
+++ service/server.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+
+	initRoutes(mx, formatter)
+
+	n.UseHandler(mx)
+	return n
+}
+
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+
+	//User Handler
+	mx.HandleFunc("/api/agenda/user/login", isLoginHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/api/agenda/user/login", loginHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/api/agenda/user/register", registerHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/api/agenda/user/logout", logoutHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/api/agenda/users", queryUsersHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/api/agenda/user/self", deleteUserHandler(formatter)).Methods("DELETE")
+
+	//Meeting Hanndler
+	mx.HandleFunc("/api/agenda/mneeting", createMeetingHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/api/agenda/meeting/{title}/participators", addParticipatorsHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/api/agenda/meeting/{title}/particioators", deleteParticipatorsHandler(formatter)).Methods("DELETE")
+	mx.HandleFunc("/api/agenda/meeting", queryMeetingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/api/agenda/meeting/sponser/{title}", cncelMeetingHandler(formatter)).Methods("DELETE")
+	mx.HandleFunc("/api/agenda/meeting/participator/{title}", quitMeetingHandler(formatter)).Methods("DELETE")
+	mx.HandleFunc("/api/agenda/meeting/sponser/all", deleteAllMeetingHandler(formatter)).Methods("DELETE")
+}
---
+++ service/user-handler.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"net/http"
+	"strconv"
+	"github.com/unrolled/render"
+)
+
+func isLoginHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request) {
+
+	}
+}
+
+
+func loginHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request){
+
+	}
+}
+
+func logoutHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request){
+
+	}
+}
+
+func registerHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request){
+
+	}
+}
+
+func deleteUserHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request){
+
+	}
+}
+
+func queryUsersHandler(formatter *render.Render) http.HandleFunc{
+	return func(w http.ResponseWriter, req *http.Request){
+		 
+	}
+}
