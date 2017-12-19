f96ae49f0e61cc4106ac72f99f6dd7f20be16803
ZhangZeMian <1106066690@qq.com>
2017-12-19 10:26:34 +0800

    session实体和service实现

--- cli/service/sessionService.go
+++ cli/service/sessionService.go
@@ -12,5 +12,24 @@ var SessionService = SessionAtomicService{}
 
 //Save
 func (*SessionAtomicService) Save(s *entity.Session) error{
-	_, err := 
+	_, err := Enginea.Insert(s)
+
+	CheckErr(err)
+
+	return nil
+}
+
+//Find
+func (*SessionAtomicService) FindSession(s *entity.Session) bool{
+	has, err := Enginea.Get(s)
+	return has
+}
+
+//Delete
+func (*SessionAtomicService) DeleteSession(s *entity.Session) error{
+	_, err := Enginea.Delete(s)
+
+	CheckErr(err)
+
+	return nil
 }
